8eaff2fdbbb62c9fca274b91f40fda5f6a959345
Anastasia <nastazka8@gmail.com>
2023-12-23 09:35:35 +0300

    добавила первую задачу

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 1: Проверка кратности числа 7 и 23
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 7 == 0 && a % 23 == 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
